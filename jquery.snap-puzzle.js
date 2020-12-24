3082f7059a4a50aee4e0e9eff3bde61c8f3b3f04
Lê Phạm Bảo Thư <70379172+Le-Pham-Bao-Thu@users.noreply.github.com>
2020-12-24 12:30:51 +0700

    Update jquery.snap-puzzle.js

--- jquery.snap-puzzle.js
+++ jquery.snap-puzzle.js
@@ -1,9 +1,4 @@
-/*
-	jQuery snapPuzzle v1.0.0
-    Copyright (c) 2014 Hans Braxmeier / Simon Steinberger / Pixabay
-    GitHub: https://github.com/Pixabay/jQuery-snapPuzzle
-	License: http://www.opensource.org/licenses/mit-license.php
-*/
+
 
 (function($){
     $.fn.snapPuzzle = function(options){
